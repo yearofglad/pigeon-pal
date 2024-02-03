3b8fc5ee6f286e07a1405bfe97513c23c6ce1940
Vinson Zhang <vinson.zhang2010@gmail.com>
2024-02-03 17:29:30 -0500

    test.py updated with animation and movement.

--- test.py
+++ test.py
@@ -4,9 +4,19 @@ import os
 from PIL import Image, ImageTk
 
 window = tk.Tk()
+x = 0
+window.geometry(f'100x100+{x}+200')  # Set window dimensions and initial position
 
-frameCount = 5
-frames = [tk.PhotoImage(file = './gifs/idle.gif', format = 'gif -index %i' %(i)) for i in range(frameCount)]
+frameCount = 8
+frames = [tk.PhotoImage(file = './gifs/idle_to_sleep.gif', format = 'gif -index %i' %(i)) for i in range(frameCount)]
+
+
+def update_position():
+    global x
+    x += 5  # Adjust the speed as needed
+    window.geometry(f'100x100+{x}+200')  # Set window dimensions and position
+    window.after(50, update_position)  # Schedule the next update after 50 milliseconds
+
 
 def update(ind):
     frame = frames[ind]
@@ -16,8 +26,10 @@ def update(ind):
     label.configure(image=frame)
     window.after(400, update, ind)
 
+
 label = tk.Label()
 label.pack()
+update_position()
 window.after(0, update, 0)
 
 window.mainloop()
